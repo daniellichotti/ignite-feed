4555fc91b74db3eeb1f50090ad7dc6b4f0bcf397
Daniel Lichotti <danielclichotti@gmail.com>
2024-07-27 13:15:21 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,21 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://plus.unsplash.com/premium_photo-1682145730713-34bba6d3d14a?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1607798748738-b15c40d33d57?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/79683513?v=4" />
+
         <strong>Daniel Lichotti</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
     
